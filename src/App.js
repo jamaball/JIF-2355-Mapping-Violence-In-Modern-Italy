ccdb22d28dd0bb6542ccfdd6c5af82f0fbcadb48
Hannah Wysocki <hwysocki22@gmail.com>
2023-01-20 16:14:28 -0500

    made it start at italy and tried to restrict bounds

--- src/App.js
+++ src/App.js
@@ -9,35 +9,40 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiaHd5c29ja2kyMiIsImEiOiJjbGQyOG1kOTIwNWVnM3hvO
 export default function App() {
   const mapContainer = useRef(null);
   const map = useRef(null);
-  const [lng, setLng] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
-  const [zoom, setZoom] = useState(9);
+  const [lng, setLng] = useState(10.8441);
+  const [lat, setLat] = useState(42.1466);
+  const [zoom, setZoom] = useState(4.5);
+  const bounds = [
+    [9, 40], // Southwest coordinates
+    [16 , 47] // Northeast coordinates
+    ];
    
   useEffect(() => {
-  if (map.current) return; // initialize map only once
-  map.current = new mapboxgl.Map({
-  container: mapContainer.current,
-  style: 'mapbox://styles/mapbox/streets-v12',
-  center: [lng, lat],
-  zoom: zoom
-  });
+    if (map.current) return; // initialize map only once
+      map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [lng, lat],
+      zoom: zoom,
+      maxBounds: bounds
+    });
   });
    
   useEffect(() => {
-  if (!map.current) return; // wait for map to initialize
-  map.current.on('move', () => {
-  setLng(map.current.getCenter().lng.toFixed(4));
-  setLat(map.current.getCenter().lat.toFixed(4));
-  setZoom(map.current.getZoom().toFixed(2));
-  });
+    if (!map.current) return; // wait for map to initialize
+    map.current.on('move', () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
   });
    
   return (
-  <div>
-  <div className="sidebar">
-  Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-  </div>
-  <div ref={mapContainer} className="map-container" />
-  </div>
-  );
+    <div>
+      <div className="sidebar">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </div>
+      <div ref={mapContainer} className="map-container" />
+    </div>
+    );
   }
