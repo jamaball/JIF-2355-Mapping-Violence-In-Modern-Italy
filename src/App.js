ba20d93fe76daced67cbd5aaee0ee8df08273bd4
Owen-Huggins <owenscotthuggins@gmail.com>
2023-01-20 14:47:11 -0500

    Co-authored-by: jamaball <jamaball@users.noreply.github.com> Co-authored-by: hwysocki22 <hwysocki22@users.noreply.github.com> Co-authored-by: excelguruson <excelguruson@users.noreply.github.com> Co-authored-by: chernandez67 <chernandez67@users.noreply.github.com> Co-authored-by: Ethan Lawrence Haarer <ethanhaarer@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,123 +1,43 @@
 import './App.css';
-import {useState, useEffect} from 'react';
-import React from 'react'
+import React, { useRef, useEffect, useState } from 'react';
 import { API_URL } from "./constants";
 import axios from "axios";
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl-csp';
 
-function App() {
-  const [description, setDescription] = useState('Click on a point to view is data here');
-  const [active, setActive] = useState(true)  
-  const [posts, setPosts] = useState([]);
+mapboxgl.accessToken = 'sk.eyJ1IjoiZWhhYXJlcjMiLCJhIjoiY2xkM285aHJxMGUzbzN2cnZ0MG1xcmIxcyJ9.PC7Ci6nV4p2JG3VDCDoMQQ';
 
+export default function App() {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+   
   useEffect(() => {
-    axios.get(API_URL)
-    .then(res => setPosts(res.data))
-    .catch(err => console.log(err));
-  }, [])
-
-  const buttons = posts.map(data => {
-    return (
-      <div id={"b" + data.id}>
-        <Button stateChanger={setDescription} data={data}/>
-      </div>
-    )
-  })
-
+  if (map.current) return; // initialize map only once
+  map.current = new mapboxgl.Map({
+  container: mapContainer.current,
+  style: 'mapbox://styles/mapbox/streets-v12',
+  center: [lng, lat],
+  zoom: zoom
+  });
+  });
+   
+  useEffect(() => {
+  if (!map.current) return; // wait for map to initialize
+  map.current.on('move', () => {
+  setLng(map.current.getCenter().lng.toFixed(4));
+  setLat(map.current.getCenter().lat.toFixed(4));
+  setZoom(map.current.getZoom().toFixed(2));
+  });
+  });
+   
   return (
-    <div className="App">
-      <header className='header'>
-        <p>GMU History Dept. Mapping Volience In Italy</p>
-      </header>
-      <div className="Map">
-        <div id="italyImg"/>
-          <div>{buttons}</div>
-        </div>    
-      <div className='DataInfo'>
-        <InfoBar isActive={active === true} desc={description}/>
-      </div>
-    </div>
+  <div>
+  <div className="sidebar">
+  Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+  </div>
+  <div ref={mapContainer} className="map-container" />
+  </div>
   );
-}
-
-function Button({stateChanger, data}) {
-  return (
-    <button className='Button' onClick={event => {stateChanger(data)}}></button> 
-  )
-}
-
-function InfoBar({desc, isActive}) {
-  let output = JSON.stringify(desc, null)
-  console.log(output)
-  return (
-    <div className="InfoBar">
-      {isActive ? (
-        <p>{output}</p>
-      ) : (
-        <p>Click on a point to view is data here</p>
-      )}
-    </div>
-  );
-}
-
-export default App;
-
-
-
-
-{/*   ***Old Snippets***
-
-const handleClick = ({ target })=> {
-  setDescription(target.desc)
-  setActive(!active)
-};
-
-<button className='button' desc="hello" onClick={handleClick}></button>
-<div id="jD1">
-  <Button stateChanger={setDescription} data={jsonData1}/> 
-</div>
-<div id="jD2">
-  <Button stateChanger={setDescription} data={jsonData2}/> 
-</div>
-<div id="jD3">
-  <Button stateChanger={setDescription} data={jsonData3}/> 
-</div>
-
-let jD1 = {
-  "Element" : "jD1",
-  "Name" : "Chris",
-  "Location" : "Venice",
-  "Weapon" : "Gold Crossbow"
-}
-
-let jD2 = {
-  "Element" : "jD2",
-  "Name" : "Ben",
-  "Location" : "Milan",
-  "Weapon" : "Blikky"
-}
-
-let jD3 = {
-  "Element" : "jD3",
-  "Name" : "John",
-  "Location" : "Turin",
-  "Weapon" : "massive sword"
-}
-
-let jsonData = [jD1, jD2, jD3];
-
-
-  axios.get(API_URL)
-  .then((res) => {
-        //console.log(res.data)
-      //pgresponse = res.data
-      buttons = res.data.map(data => {
-        console.log(data.id)
-        return (
-          <div id={data.id}>
-            <Button stateChanger={setDescription} data={data}/>
-          </div>
-        )
-      })
-  })
-
-*/}
+  }
