9ee349bd1e49fc56ca5bd35045a0f90095f3672f
jamaball <104403889+jamaball@users.noreply.github.com>
2023-01-26 18:24:47 -0500

    Iteration 1
    Working mapBox model with clickable datapoints

--- src/App.js
+++ src/App.js
@@ -1,51 +1,9 @@
 import './App.css';
-
-import React, { useRef, useEffect, useState } from 'react';
-import { API_URL } from "./constants";
-import axios from "axios";
-import mapboxgl from 'mapbox-gl';
-
-mapboxgl.accessToken = 'pk.eyJ1IjoiaHd5c29ja2kyMiIsImEiOiJjbGQyOG1kOTIwNWVnM3hvOW15a2syMnFqIn0.X5H6aAIVGej-R6QVWx4LVg';
+import React from 'react';
+import Map from './Map.jsx';
 
 export default function App() {
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(10.8441);
-  const [lat, setLat] = useState(42.1466);
-  const [zoom, setZoom] = useState(4.5);
-  const bounds = [
-      [9, 40], // Southwest coordinates
-      [16 , 47] // Northeast coordinates
-    ];
-   
-  useEffect(() => {
-    if (map.current) return; // initialize map only once
-      map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v12',
-      center: [lng, lat],
-      zoom: zoom,
-      maxBounds: bounds
-    });
-  });
-   
-  useEffect(() => {
-    if (!map.current) return; // wait for map to initialize
-    map.current.on('move', () => {
-      setLng(map.current.getCenter().lng.toFixed(4));
-      setLat(map.current.getCenter().lat.toFixed(4));
-      setZoom(map.current.getZoom().toFixed(2));
-    });
-  });
-   
-  return (
-    <div>
-      <div className="sidebar">
-        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-      </div>
-      <div ref={mapContainer} className="map-container" />
-    </div>
-  );
+  return <Map/>
 }
 
   /*   ***Old Snippets***
