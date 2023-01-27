c1a2c92804ecc186e43e95b71b9b20e1fd36a4ee
Ethan Lawrence Haarer <ethanhaarer@gmail.com>
2023-01-27 15:30:04 -0500

    Added Map Bounds
    Added map bounds to the map to restrict the world to italy

--- src/Map.jsx
+++ src/Map.jsx
@@ -28,6 +28,7 @@ const Map = () => {
       style: "mapbox://styles/mapbox/streets-v11",
       center: [13, 43],
       zoom: 5,
+      maxBounds: [[4.91306038378405, 36.08567211105813], [19.225508855943896, 48.79804811867416]]
     });
 
     // Render custom marker components
