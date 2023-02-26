bfc7673dd999f3ce97dbd911b0f91e7dda53e419
Ethan Lawrence Haarer <ethanhaarer@gmail.com>
2023-02-25 23:45:20 -0500

    Updated All Inputs
    Overhauled the UI and CSS for all styles of input both for the filtering system and the log in page, so now the whole site looks much more polished. However, still need to implements filtering from old button systems into the new checkmark system by entering the submit button

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -5,14 +5,19 @@ const LoginPage = () => {
     <div class = "LoginAlign">
       <br></br>
       <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
       <h1>Login Page</h1>
       <br></br>
       <br></br>
       <h2>Username</h2>
-      <input />
+      <input id = "inputRoundedEdge"/>
+      <br></br>
       <br></br>
       <h2>Password</h2>
-      <input />
+      <input id = "inputRoundedEdge"/>
       <br></br>
       <br></br>
       <button class = "Button">New Account</button>
