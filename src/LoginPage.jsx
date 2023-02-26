f9f0d6d80747988abe24f60fbb07b49c3800ba0b
Ethan Lawrence Haarer <ethanhaarer@gmail.com>
2023-02-25 20:15:57 -0500

    Fixed Page Navigation
    Integrated new UI with the multiple pages so that the new log in page

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -2,12 +2,15 @@ import React from "react";
   
 const LoginPage = () => {
   return (
-    <div>
+    <div class = "LoginAlign">
       <h1>Login Page</h1>
       <h2>Username</h2>
       <input />
       <h2>password</h2>
       <input />
+      <br></br>
+      <button>Submit</button>
+      <button>New Account</button>
     </div>
   );
 };
