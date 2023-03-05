3a7842a08b4346c5fd9f5e32a667c568f17a1421
Hannah Wysocki <hwysocki22@gmail.com>
2023-03-05 17:46:50 -0500

    Created login backend functionality

--- mvproject/mvproject/settings.py
+++ mvproject/mvproject/settings.py
@@ -12,18 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
  
-GDAL_LIBRARY_PATH = r'C:\OSGeo4W64\bin\gdal306'
-import os
-if os.name == 'nt':
-    import platform
-    OSGEO4W = r"C:\OSGeo4W"
-    if '64' in platform.architecture()[0]:
-        OSGEO4W += "64"
-    assert os.path.isdir(OSGEO4W), "Directory does not exist: " + OSGEO4W
-    os.environ['OSGEO4W_ROOT'] = OSGEO4W
-    os.environ['GDAL_DATA'] = OSGEO4W + r"\share\gdal"
-    os.environ['PROJ_LIB'] = OSGEO4W + r"\share\proj"
-    os.environ['PATH'] = OSGEO4W + r"\bin;" + os.environ['PATH']
 
 
 from pathlib import Path
@@ -49,6 +37,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'crimes',
     'rest_framework',
+    'knox',
+    'account',
     'rest_framework_gis',
     'corsheaders',
 
@@ -62,6 +52,10 @@ INSTALLED_APPS = [
     'django.contrib.gis',
 
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',)
+}
 
 CORS_ORIGIN_ALLOW_ALL = True
 
