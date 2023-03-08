98a31f9c871ac62718dcdd67cb8fb929fc5a70c2
chernandez67 <chernandez67@gatech.edu>
2023-03-07 19:28:41 -0500

    Fix reset button scope

--- src/Map.jsx
+++ src/Map.jsx
@@ -225,6 +225,15 @@ zoom: 11.15
         const layers = document.getElementById('menu');
         layers.appendChild(link);
         }
+
+        document.getElementById('reset').addEventListener('click', function() {
+          var sample = featureCollection([]);
+          filteredData = responseData;
+          sample.features = responseData.features;
+          activeLinks = []
+          map.getSource('myData').setData(sample);
+          document.getElementById('active-year').innerText = 1700;
+        });
       });
 
         map.on('click', 'clusters', (e) => {
@@ -319,14 +328,7 @@ zoom: 11.15
         map.getSource('myData').setData(sample);
       });
 
-      document.getElementById('reset').addEventListener('click', function() {
-        var sample = featureCollection([]);
-        filteredData = responseData;
-        sample.features = responseData.features;
-        activeLinks = []
-        map.getSource('myData').setData(sample);
-        document.getElementById('active-year').innerText = 1700;
-      });
+
 
       document.getElementById('download').addEventListener('click', function() {
         const element = document.createElement("a");
